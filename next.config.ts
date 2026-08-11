d4633c3ba9a6f303e14a6db20ae28f64cc732e7a
Jack Wilde <wildejack1010@gmail.com>
2026-08-11 12:47:26 -0600

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const isGithubPages = process.env.GITHUB_PAGES === "1";
+
+const nextConfig: NextConfig = {
+  output: isGithubPages ? "export" : undefined,
+  basePath: isGithubPages ? "/locker-app" : undefined,
+  assetPrefix: isGithubPages ? "/locker-app/" : undefined,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
